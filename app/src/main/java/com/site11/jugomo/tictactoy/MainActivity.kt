6845f8b70d546004178b28bf45d93ce6998468f6
jugomo <julio@Julios-MacBook.local>
2017-08-26 20:57:25 +0200

    add game reset
    - bug fixes
    - add game reset
    - add status message
    - support SDK >= 16

--- app/src/main/java/com/site11/jugomo/tictactoy/MainActivity.kt
+++ app/src/main/java/com/site11/jugomo/tictactoy/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     var player1 = ArrayList<Int>()
     var player2 = ArrayList<Int>()
     var active = 1
+    var finished = false
 
     //
     //
@@ -22,27 +23,72 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btReset.isEnabled = false
+    }
+
+    fun resetAction(view: View) {
+
+        bt1.setBackgroundColor(resources.getColor(android.R.color.darker_gray))
+        bt2.setBackgroundColor(resources.getColor(android.R.color.darker_gray))
+        bt3.setBackgroundColor(resources.getColor(android.R.color.darker_gray))
+        bt4.setBackgroundColor(resources.getColor(android.R.color.darker_gray))
+        bt5.setBackgroundColor(resources.getColor(android.R.color.darker_gray))
+        bt6.setBackgroundColor(resources.getColor(android.R.color.darker_gray))
+        bt7.setBackgroundColor(resources.getColor(android.R.color.darker_gray))
+        bt8.setBackgroundColor(resources.getColor(android.R.color.darker_gray))
+        bt9.setBackgroundColor(resources.getColor(android.R.color.darker_gray))
+
+        bt1.setText("")
+        bt2.setText("")
+        bt3.setText("")
+        bt4.setText("")
+        bt5.setText("")
+        bt6.setText("")
+        bt7.setText("")
+        bt8.setText("")
+        bt9.setText("")
+
+        bt1.isEnabled = true
+        bt2.isEnabled = true
+        bt3.isEnabled = true
+        bt4.isEnabled = true
+        bt5.isEnabled = true
+        bt6.isEnabled = true
+        bt7.isEnabled = true
+        bt8.isEnabled = true
+        bt9.isEnabled = true
+
+        btReset.isEnabled = false
+        finished = false
+        player1.clear()
+        player2.clear()
+
+        tvMessage.text = "Welcome!"
+        tvMessage.setTextColor(Color.BLACK)
     }
 
     fun btClick(view: View) {
-        val btSel = view as Button
+        if (!finished) {
+            val btSel = view as Button
 
-        var cellId = 0
-        when(btSel.id) {
-            R.id.bt1 -> cellId = 1
-            R.id.bt2 -> cellId = 2
-            R.id.bt3 -> cellId = 3
-            R.id.bt4 -> cellId = 4
-            R.id.bt5 -> cellId = 5
-            R.id.bt6 -> cellId = 6
-            R.id.bt7 -> cellId = 7
-            R.id.bt8 -> cellId = 8
-            R.id.bt9 -> cellId = 9
+            var cellId = 0
+            when (btSel.id) {
+                R.id.bt1 -> cellId = 1
+                R.id.bt2 -> cellId = 2
+                R.id.bt3 -> cellId = 3
+                R.id.bt4 -> cellId = 4
+                R.id.bt5 -> cellId = 5
+                R.id.bt6 -> cellId = 6
+                R.id.bt7 -> cellId = 7
+                R.id.bt8 -> cellId = 8
+                R.id.bt9 -> cellId = 9
+            }
+
+            //Toast.makeText(this, "ID: " + cellId, Toast.LENGTH_LONG).show()
+
+            playGame(cellId, btSel)
         }
-
-        //Toast.makeText(this, "ID: " + cellId, Toast.LENGTH_LONG).show()
-
-        playGame(cellId, btSel)
     }
 
     fun playGame(cellID: Int, sel: Button) {
@@ -93,7 +139,6 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-
         // col 1
         if (player1.contains(1) && player1.contains(4) && player1.contains(7)) {
             winner = 1
@@ -118,16 +163,56 @@ class MainActivity : AppCompatActivity() {
             winner = 2
         }
 
+        // there is a winner
         if (winner != -1) {
             if (winner == 1) {
-                Toast.makeText(this, "PLAYER 1 WON!", Toast.LENGTH_LONG).show()
+                tvMessage.text = "YOU WON!"
+                tvMessage.setTextColor(Color.GREEN)
             } else {
-                Toast.makeText(this, "PLAYER 2 WON!", Toast.LENGTH_LONG).show()
+                tvMessage.text = "PLAYER 2 WON!"
+                tvMessage.setTextColor(Color.RED)
+            }
+
+            btReset.isEnabled = true
+            finished = true
+        } else {
+            if (emptyCells().size == 0) {
+                gameOver()
             }
         }
     }
 
     fun autoPlay() {
+        val num = emptyCells()
+
+        if (num.size != 0) {
+            val r = Random()
+            val randIndex = r.nextInt(num.size - 0) + 0
+            val cellID = num.get(randIndex)
+
+            var btSelect: Button?
+            when (cellID) {
+                1 -> btSelect = bt1
+                2 -> btSelect = bt2
+                3 -> btSelect = bt3
+                4 -> btSelect = bt4
+                5 -> btSelect = bt5
+                6 -> btSelect = bt6
+                7 -> btSelect = bt7
+                8 -> btSelect = bt8
+                9 -> btSelect = bt9
+                else -> {
+                    btSelect = bt1
+                }
+            }
+
+            playGame(cellID, btSelect)
+        } else {
+            gameOver()
+        }
+    }
+
+    fun emptyCells() : ArrayList<Int> {
         var emptyCells = ArrayList<Int>()
 
         for (cellID in 1..9) {
@@ -136,26 +221,13 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        val r = Random()
-        val randIndex = r.nextInt(emptyCells.size - 0) + 0
-        val cellID = emptyCells.get(randIndex)
+        return emptyCells
+    }
 
-        var btSelect: Button?
-        when(cellID) {
-            1 -> btSelect = bt1
-            2 -> btSelect = bt2
-            3 -> btSelect = bt3
-            4 -> btSelect = bt4
-            5 -> btSelect = bt5
-            6 -> btSelect = bt6
-            7 -> btSelect = bt7
-            8 -> btSelect = bt8
-            9 -> btSelect = bt9
-            else -> {
-                btSelect = bt1
-            }
-        }
-
-        playGame(cellID, btSelect)
+    fun gameOver() {
+        tvMessage.text = "GAME OUT! - NOBODY WON"
+        tvMessage.setTextColor(Color.RED)
+        btReset.isEnabled = true
+        finished = true
     }
 }
